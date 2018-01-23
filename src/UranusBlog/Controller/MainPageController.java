e95191189ec16db5c2ddc25959fd7e3e1e45c7f9
louis297 <xl297@georgetown.edu>
2018-01-23 17:30:56 +1300

    servlet setup

--- src/UranusBlog/Controller/MainPageController.java
+++ src/UranusBlog/Controller/MainPageController.java
@@ -1,16 +1,19 @@
 package UranusBlog.Controller;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class IndexController extends HttpServlet {
+public class MainPageController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        PrintWriter out = resp.getWriter();
+        out.println("Hello world!");
     }
 
     @Override
