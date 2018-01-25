87e62d9d76ad04085ee30fa578cf9e6dd61df3b9
louis297 <xl297@georgetown.edu>
2018-01-25 17:59:46 +1300

    get data from DB in main.html

--- web/static/js/article/articleList.js
+++ web/static/js/article/articleList.js
@@ -1,18 +1,52 @@
 function getArticleList(all) {
-    if (all) {
-        var ret = []
-        for (var i = 0; i < 10; i++) {
-            var single = {};
-            single.title = "mock title" + i
-            single.content = "mock content" + i
-            single.author = "mock author" + i
-            d = new Date()
-            single.createTime = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
-            single.modifiedTime = d.toLocaleDateString() + ' ' + d.toLocaleTimeString();
-            ret.push(single)
+    var params = {own: all};
+    $.ajax({
+        url: '/articleList',
+        method: 'get',
+        data: params,
+        success: function(ret) {
+            parsedData = [];
+            console.log(ret);
+            allData = JSON.parse(ret);
+            for(var i=0; i<allData.length; i++){
+                var single={};
+                single.title = allData[i].title
+                single.content = allData[i].content;
+                // todo: if not all get author name from session
+                if(all) {
+                    single.author = allData[i].authorName;
+                }
+                single.createTime = allData[i].created_time;
+                single.modifiedTime = allData[i].modified_time;
+                parsedData.push(single)
+            }
+            updateArticles(parsedData);
         }
-        return ret
-    } else {
+    });
 
-    }
+}
+
+function updateArticles(articleList){
+    var i=0;
+    articleList.forEach(function(article){
+        var ad = $('<div id="article' + i + '" class="articleBox"></div>')
+        ad.append('<div class="articleTitle" id="articleTitle' + i +'">' +
+            article.title +
+            '</div>');
+        ad.append('<div id="author"' + i + '>' +
+            article.author +
+            '</div>')
+        ad.append('<div id="create_time"' + i + '>' +
+            article.createTime +
+            '</div>')
+        ad.append('<div id="modify_time"' + i + '>' +
+            article.modifiedTime +
+            '</div>')
+        ad.append('<div class="articleContent" id="articleContent' + i +'">' +
+            article.content +
+            '</div>');
+
+        i++;
+        $('#articleList').append(ad)
+    });
 }
--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,27 +1,4 @@
-function updateArticles(articleList){
-    var i=0;
-    articleList.forEach(function(article){
-        var ad = $('<div id="article' + i + '" class="articleBox"></div>')
-        ad.append('<div class="articleTitle" id="articleTitle' + i +'">' +
-            article.title +
-            '</div>');
-        ad.append('<div id="author"' + i + '>' +
-            article.author +
-            '</div>')
-        ad.append('<div id="create_time"' + i + '>' +
-            article.createTime +
-            '</div>')
-        ad.append('<div id="modify_time"' + i + '>' +
-            article.modifiedTime +
-            '</div>')
-        ad.append('<div class="articleContent" id="articleContent' + i +'">' +
-            article.content +
-            '</div>');
 
-        i++;
-        $('#articleList').append(ad)
-    });
-}
 
 function loginAction(){
     var username = $('#Username').val();
@@ -48,9 +25,6 @@ $(function () {
     var loginBtn = $('#loginButton');
     loginBtn.click(loginAction);
 
-
-    var articleList = getArticleList(true)
-    console.log(articleList)
-    updateArticles(articleList)
+    getArticleList(true)
 
 })
