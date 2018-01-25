f5628bd0516f7fd8d1864fb60629b11a055072e9
louis297 <xl297@georgetown.edu>
2018-01-25 17:18:32 +1300

    update GetArticleById SP

--- src/UranusBlog/sql/sp_GetArticleById.sql
+++ src/UranusBlog/sql/sp_GetArticleById.sql
@@ -1,10 +1,12 @@
 DROP PROCEDURE IF EXISTS GetArticleById;
 
 DELIMITER ##
-CREATE PROCEDURE GetArticleById(in uid int(11), IN aid int(11))
+CREATE PROCEDURE GetArticleById(IN userId INT(11), IN aid INT(11))
   BEGIN
-    SELECT * from article
-    WHERE active = 1 and article_id = aid and (private = 0 or (private = 1 and author = uid));
+    SELECT article.*, `account`.username AS author_name FROM article, `account`
+    WHERE active = 1 AND article_id = aid AND ((private = 0 AND post_time < current_time) OR (private = 1 AND author = userId))
+    AND author = `account`.uid
+    GROUP BY `article_id`;
   END ##
 
 DELIMITER ;
